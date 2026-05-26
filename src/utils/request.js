f9362f2e9ef8738fbe5bde7d4a0b64343c074574
wuyuanjun <wyj10242025@163.com>
2026-05-27 00:39:02 +0800

    将请求等待时间从1分钟改为2分钟

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const request = axios.create({
   baseURL: "https://wyj-travel-assistant-sever2-262567-8-1437172584.sh.run.tcloudbase.com/api/travel",
-  timeout: 60000,
+  timeout: 120000,
   headers: {
     "Content-Type": "application/json",
   },
