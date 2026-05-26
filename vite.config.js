299c52ce2a4d9d0a936313d794a66c8cf6903aba
wuyuanjun <wyj10242025@163.com>
2026-05-27 00:26:59 +0800

    修改访问域名为腾讯云Travel-h5域名

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import vue from "@vitejs/plugin-vue";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "/", // 部署到根域名时填 '/'
+  base: "/Travel-h5/", // 部署到Travel-h5域名时填
   build: { outDir: "dist" },
 });
